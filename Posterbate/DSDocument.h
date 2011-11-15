adb1dd63e78c38006f4c950459c34d9c75d6fc20
David Selassie <selassid@gmail.com>
2011-11-14 23:01:35 -0800

    Initial Commit

---
+++ Posterbate/DSDocument.h
@@ -0,0 +1,13 @@
+//
+//  DSDocument.h
+//  Posterbate
+//
+//  Created by David Selassie on 11/14/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DSDocument : NSDocument
+
+@end
